8d7a7c724ce29aa1c8956301e744decc714747a2
Krishna Teja <shaadomanthra@yahoo.com>
2019-09-01 05:56:53 +0000

    some changes

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'noreply@prep.firstacademy.in'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@firstacademy.co.in'),
         'name' => env('MAIL_FROM_NAME', 'First Academy'),
     ],
 
