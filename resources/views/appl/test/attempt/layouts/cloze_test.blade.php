2cadd6ba47a5f6f8575aa171b2db281782237ac1
Krishna Teja <shaadomanthra@yahoo.com>
2019-09-10 08:22:53 +0000

    some changes

--- resources/views/appl/test/attempt/layouts/cloze_test.blade.php
+++ resources/views/appl/test/attempt/layouts/cloze_test.blade.php
@@ -1,16 +1,17 @@
-
 <span class="question " id="{{$f->qno}}">
       @if($f->prefix ) {{$f->prefix }}  @endif 
       @if($f->answer)
       &nbsp;
-      <span class="badge badge-warning h2">{{$f->qno}}</span> &nbsp;
+      <span style="display:inline-block;">
+      <span class="badge badge-warning h2">{{$f->qno}}</span>
       <select class=" input fill" name="{{$f->qno}}" data-id="{{$f->qno}}">
       <option value=""></option>
       @foreach(explode('/',$f->label) as $option)
       <option value="{{ trim($option) }}">{{$option}}</option>
       @endforeach   
       </select>
-      &nbsp;
+    </span>
+      &nbsp; 
       @endif
       @if($f->suffix ){{$f->suffix }} @endif
 </span>
