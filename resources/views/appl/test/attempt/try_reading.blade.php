86140d7d7451dcbc6d9b447ec597d43658073542
Krishna Teja <shaadomanthra@yahoo.com>
2019-09-12 06:45:42 +0000

    some changes

--- resources/views/appl/test/attempt/try_reading.blade.php
+++ resources/views/appl/test/attempt/try_reading.blade.php
@@ -34,7 +34,7 @@
 
         <input type="hidden" name="test_id" value="{{ $app->test->id }}">
         <input type="hidden" name="user_id" value="{{ \auth::user()->id }}">
-        <input type="hidden" name="product" value="{{ $product->slug }}">
+        <input type="hidden" name="product" value="@if($product){{ $product->slug }} @endif">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         @include('appl.test.attempt.blocks.qno_reading')
 
