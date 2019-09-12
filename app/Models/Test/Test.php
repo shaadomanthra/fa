377c9476add84c6592d696232e4f765010863c16
Krishna Teja <shaadomanthra@yahoo.com>
2019-09-12 12:19:17 +0000

    some changes

--- app/Models/Test/Test.php
+++ app/Models/Test/Test.php
@@ -67,6 +67,7 @@ class Test extends Model
             return null;
 
         $orders = $user->orders()->whereIn('product_id',$this->products->pluck('id')->toArray())->orWhere('test_id',$this->id)->orderBy('id','desc')->get();
+        dd($orders);
 
         foreach($orders as $order){
             if($order->status){
