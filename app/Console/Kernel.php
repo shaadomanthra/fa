565945b8358de7d91963cd0a26afda956fe047f0
Krishna Teja <shaadomanthra@yahoo.com>
2019-09-19 12:33:02 +0000

    some changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('day:update')
-                    ->twiceDaily(13,18);
+                    ->everyMinute();
     }
 
     /**
