46f43ce42406a15990090fe335a0c3e11a532551
Krishna Teja <shaadomanthra@yahoo.com>
2019-10-23 09:50:48 +0000

    Some changes

--- resources/views/appl/test/attempt/layouts/ielts_example.blade.php
+++ resources/views/appl/test/attempt/layouts/ielts_example.blade.php
@@ -4,18 +4,17 @@
 
   @if($f->label)
   <div class="col-12 col-md-4" id="{{$f->qno}}">
-    <div class="card-text " ><b>{{ $f->label}}</b>
-    </div>
+    <div class="card-text " ><b>{{ $f->label}}</b></div>
   </div>
  @endif
   <div class="col-12 col-md">
 
       @if($f->prefix ) {{$f->prefix }}  @endif 
-      @if($f->answer) <input type="text" class="fill input mb-0"  value="{{$f->answer}}" disabled>
+      @if($f->answer) 
+      <input type="text" class="fill input mb-0"  value="{{$f->answer}}" disabled>
       @endif
       @if($f->suffix ){{$f->suffix }}@endif
     
   </div>
-</div>
 </div>
 </div>
