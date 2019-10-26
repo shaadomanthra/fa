813719093dbc31cfac10f04c03478b88e70b1a94
Krishna Teja <shaadomanthra@yahoo.com>
2019-10-26 07:24:47 +0000

    some changes

---
+++ resources/views/appl/product/product/blocks/header_products.blade.php
@@ -0,0 +1,20 @@
+<div class=" container ">
+  <div class="row pt-4 pb-4">
+    <div class="col-12 col-md-8">
+      <h1 class="h3 mb-0 mt-2"><b><i class="fa fa-cubes"></i> Products </b></h1>
+    </div>
+
+    <div class="col-12 col-md-4">
+      <form class="form-inline float-right" method="GET" action="{{ route($app->module.'.public') }}">
+            <div class="input-group input-group-lg">
+              <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fa fa-search"></i></div>
+              </div>
+              <input class="form-control " id="search" name="item" autocomplete="off" type="search" placeholder="Search" aria-label="Search" 
+              value="{{Request::get('item')?Request::get('item'):'' }}">
+            </div>
+          </form>
+    </div>
+  </div>
+  
+</div>
--- resources/views/appl/product/product/public.blade.php
+++ resources/views/appl/product/product/public.blade.php
@@ -1,28 +1,22 @@
-@extends('layouts.app')
+@extends('layouts.breadcrumb')
 @section('title', 'First Academy - The best practice tests for IELTS | OET and other tests')
 @section('description', 'Assess your level completely free. Free full-length tests for OET and IELTS and tests on vocabulary resources')
 @section('keywords', 'IELTS Practice Test, OET Practice Online, OET Online Training, Vocabulary for IELTS, Vocabulary for OET')
 @section('content')
 @include('flash::message')
-<div  class="row ">
-  <div class="col-12 col-md-12">
-    <nav class="navbar navbar-light  p-4 rounded justify-content-between  mb-4" style="background:#e8ecef;border:1px solid #cfd5da;">
-          <a class=" h2 mb-3 mb-md-0"><i class="fa fa-cubes"></i> Products </a>
-          <form class="form-inline" method="GET" action="{{ route($app->module.'.public') }}">
-            <div class="input-group input-group-lg">
-              <div class="input-group-prepend">
-                <div class="input-group-text"><i class="fa fa-search"></i></div>
-              </div>
-              <input class="form-control " id="search" name="item" autocomplete="off" type="search" placeholder="Search" aria-label="Search" 
-              value="{{Request::get('item')?Request::get('item'):'' }}">
-            </div>
-          </form>
-        </nav>
-    <div class="">
-      <div class=" ">
-        <div id="search-items" class="row">
+<div  class=" ">
+  <div class="">
+    <div class="" style="background: #eee">
+    @include('appl.product.product.blocks.header_products')
+    </div>
+
+    
+    <div class="bg-white">
+      <div class="container pt-4">
+        <div id="search-items" class="row ">
          @include('appl.'.$app->app.'.'.$app->module.'.public_list')
        </div>
+       <br>
 
      </div>
    </div>
