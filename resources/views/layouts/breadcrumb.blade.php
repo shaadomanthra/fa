69a49ff6d196ef8a7d0fbf985762e1dfb7737846
vagrant <vagrant@homestead>
2019-11-11 05:42:15 +0000

    some changes

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -9,7 +9,7 @@
     <link rel="manifest" href="/site.webmanifest">
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#ffc40d">
-    <meta name="theme-color" content="#1a4248">
+    <meta name="theme-color" content="#71bce2">
     <!-- CSRF Token -->
     <meta name="description" content="@yield('description')">
     <meta name="keywords" content="@yield('keywords')">
