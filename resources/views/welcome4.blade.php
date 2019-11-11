d75e6e77b0b37fa077c0745cba0fc83e2ffd9994
vagrant <vagrant@homestead>
2019-11-11 07:55:34 +0000

    some changes

--- resources/views/welcome4.blade.php
+++ resources/views/welcome4.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.front')
-@section('title', 'Tests | First Academy')
-@section('description', 'Take a free IELTS | OET test completely free. Full-length OET practice test for free! Free IELTS writing band scores. Test your vocabulary for OET and IELTS.')
+@section('title', 'First Academy - The best practice tests for IELTS | OET and other tests')
+@section('description', 'Take a free IELTS | OET test completely free. Full-length OET practice test for free! Free IELTS writing band scores. Test your vocabulary for OET and IELTS.'
 @section('keywords', 'IELTS Practice Test, OET Practice Online, OET Online Training, Vocabulary for IELTS, Vocabulary for OET')
 @section('content')
 
