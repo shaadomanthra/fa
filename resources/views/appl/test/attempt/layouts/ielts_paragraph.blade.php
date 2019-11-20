bbc23d13bafd4771e7bf2e9630b90cc0ed3c7640
vagrant <vagrant@homestead>
2019-11-20 08:16:46 +0000

    some changes

--- resources/views/appl/test/attempt/layouts/ielts_paragraph.blade.php
+++ resources/views/appl/test/attempt/layouts/ielts_paragraph.blade.php
@@ -1,14 +1,20 @@
 <span class="card-text question" id="{{$f->qno}}">
   @if($f->label ) <div><b>{{$f->label }}</b></div>  @endif 
 
-  	@if($f->layout == 'default' ||  !$f->layout)
-      @if($f->prefix ) {{$f->prefix }}  @endif 
-	  @if($f->answer) <span class="badge badge-warning h2">{{$f->qno}}</span><input type="text" class="fill input" name="{{$f->qno}}" data-id="{{$f->qno}}" >
-	  @endif
-	  @if($f->suffix ){{$f->suffix }}@endif
+  	@if($f->layout == 'default' ||  !$f->layout )
+       @if($f->prefix ) {{$f->prefix }}  @endif 
+	     @if($f->answer) <span class="badge badge-warning h2">{{$f->qno}}</span><input type="text" class="fill input" name="{{$f->qno}}" data-id="{{$f->qno}}" >
+	     @endif
+	     @if($f->suffix ){{$f->suffix }}@endif
+    @elseif($f->layout=='paragraph')
+       @if($f->prefix ) {{$f->prefix }}  @endif 
+       @if($f->answer) <span class="badge badge-warning h2">{{$f->qno}}</span><input type="text" class="fill input" name="{{$f->qno}}" data-id="{{$f->qno}}" >
+       @endif
+       @if($f->suffix ){{$f->suffix }}@endif
     @elseif($f->layout=='ielts_two_blank')
       <span class="badge badge-warning h2">{{$f->qno}}</span>
       @include('appl.test.attempt.layouts.ielts_two_blank') 
+
     @endif
 
   
