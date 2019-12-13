672ee1d33975c6c14a5bd33a52eb4859c28daf76
vagrant <vagrant@homestead>
2019-12-13 06:46:46 +0000

    some change

--- resources/views/appl/test/attempt/blocks/writing_file.blade.php
+++ resources/views/appl/test/attempt/blocks/writing_file.blade.php
@@ -3,12 +3,16 @@
 	<div class="bg-light border rounded p-3 mb-3">
 	{!! $attempt->response !!}
 </div>
-	<p>Good job ! Your  Writing task is submitted. Get it evaluated. </p>
+	
 	@if($attempt->answer)
+	<div class="bg-light border p-3 rounded">
+	<p>Your  writing task is evaluated.  </p>
 	<a href="{{ route('test.review',$test->slug)}}?product=@if($product){{$product->slug}}@endif">
-	<button type="button" class="btn btn-sm btn-success">Expert Review</button>
+	<button type="button" class="btn btn-bg btn-success">Expert Review</button>
 	</a>
+	</div>
 	@else
+	<p>Good job ! Your  Writing task is submitted. Get it evaluated. </p>
 	<a href="{{ route('product.view','writing-evaluation')}}">
 	<button type="button" class="btn btn-sm btn-outline-primary">Expert Evaluation</button>
 	</a>
