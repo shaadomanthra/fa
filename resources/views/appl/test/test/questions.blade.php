bf99442d9854c54f6e684fae6e2483a24c545cfd
vagrant <vagrant@homestead>
2019-12-22 08:42:35 +0000

    Some changes

--- resources/views/appl/test/test/questions.blade.php
+++ resources/views/appl/test/test/questions.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.app')
+
+@section('title', 'Question List | First Academy')
+@section('description', 'Take a free IELTS | OET test completely free. Full-length OET practice test for free! Free IELTS writing band scores. Test your vocabulary for OET and IELTS.')
+
 @section('content')
 
 <nav aria-label="breadcrumb">
@@ -19,7 +23,7 @@
     <div class="card mb-3 mb-md-0">
       <div class="card-body mb-0">
         <nav class="navbar navbar-light bg-light justify-content-between border mb-3">
-          <a class="navbar-brand"><i class="fa fa-bars"></i>  Questions </a>
+          <a class="navbar-brand"><i class="fa fa-bars"></i>  Questions ({{count($data)}})</a>
 
           <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
 
