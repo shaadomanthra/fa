529b769c2c1d7eb150eeea714a30f40d32f3eda1
vagrant <vagrant@homestead>
2020-02-26 08:52:51 +0000

    some changes:

--- resources/views/appl/pages/blocks/testlist.blade.php
+++ resources/views/appl/pages/blocks/testlist.blade.php
@@ -7,9 +7,10 @@
     <tr class="bg-light">
       <th scope="col">#</th>
       <th scope="col">Test Name</th>
-      <th scope="col">Valid Till</th>
+      
       <th scope="col">Action</th>
       <th scope="col">Status</th>
+      <th scope="col">Valid Till</th>
     </tr>
   </thead>
   <tbody>
@@ -17,7 +18,7 @@
 	<tr>
       <th scope="row">{{$k+1}}</th>
       <td><a href="{{ route('test',$test->slug)}}">{{$test->name}}</a></td>
-      <td>{{ date('d M Y', strtotime($expiry[$test->id]))}}</td>
+      
       
       <td>@if($status[$test->id]=='Active')
 			<a href="{{ route('test.instructions',$test->slug) }}">
@@ -34,6 +35,7 @@
 		<td>
       	{{ $status[$test->id]}}
       </td>
+      <td>{{ date('d M Y', strtotime($expiry[$test->id]))}}</td>
     </tr>		
 	@endforeach
   </tbody>
--- resources/views/appl/test/attempt/blocks/writing_file.blade.php
+++ resources/views/appl/test/attempt/blocks/writing_file.blade.php
@@ -1,20 +1,25 @@
+
+
 <div class="rounded p-4 mb-4" style="">
-	<h4 class="mb-3 text-primary">Your Response </h4>
-	<div class="bg-light border rounded p-3 mb-3">
-	{!! $attempt->response !!}
-</div>
-	
 	@if($attempt->answer)
-	<div class="bg-light border p-3 rounded">
-	<p>Your  writing task is evaluated.  </p>
+	<div class="alert alert-warning alert-important mb-4 border border-warning">
+	<p>Your Evaluation Is Ready!  </p>
 	<a href="{{ route('test.review',$test->slug)}}?product=@if($product){{$product->slug}}@endif">
-	<button type="button" class="btn btn-bg btn-success">Expert Review</button>
+	<button type="button" class="btn btn-bg btn-success">View Expert Review</button>
 	</a>
 	</div>
 	@else
-	<p>Good job ! Your  Writing task is submitted. Get it evaluated. </p>
-	<a href="{{ route('product.view','writing-evaluation')}}">
-	<button type="button" class="btn btn-sm btn-outline-primary">Expert Evaluation</button>
-	</a>
+	<div class="alert alert-warning alert-important mb-4 border border-warning" role="alert">
+  	Your writing task has been submitted. Please check back later for the evaluation. Good Luck! 
+	</div>
+	
 	@endif
+	
+
+	<h4 class="mb-3 text-primary">Your Response </h4>
+	<div class="bg-light border rounded p-3 mb-3">
+	{!! $attempt->response !!}
+	</div>
+	
+	
 </div>
