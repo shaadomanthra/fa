a5f8d81fe53e21dd6c9c0a164a08ba21a33c5bb9
vagrant <vagrant@homestead>
2020-03-24 05:32:39 +0000

    some changes

--- resources/views/appl/test/attempt/blocks/section_english.blade.php
+++ resources/views/appl/test/attempt/blocks/section_english.blade.php
@@ -2,7 +2,7 @@
 <div class="mb-3">
 	<div class="part">
 	<h3><i class="fa fa-clone"></i> {{ $section->name}}</h3>
-	@if(strip_tags($section->instructions))
+	@if(trim(strip_tags($section->instructions)))
 	<p>{!! $section->instructions !!}</p>
 	@endif
 	</div>
