1d51dd5575bac860e0f679c520ee075fd8ec51f9
vagrant <vagrant@homestead>
2020-04-06 08:21:47 +0000

    some changes

--- resources/views/layouts/cover.blade.php
+++ resources/views/layouts/cover.blade.php
@@ -11,35 +11,13 @@
    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
    <link href="{{ asset('css/cover.css') }}" rel="stylesheet">
 
-
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-    <!-- Custom styles for this template -->
-    <link href="cover.css" rel="stylesheet">
   </head>
   <body class="text-center">
     <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
 
-
   <main role="main" class="inner cover mt-5">
     @yield('content')
   </main>
-
   <footer class="mastfoot mt-auto">
     <div class="inner">
       <p>
