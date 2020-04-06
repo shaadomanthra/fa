9972f8b52f6287dfcc2cca9b0c05b08b2bbbdc6b
vagrant <vagrant@homestead>
2020-04-06 05:53:22 +0000

    some changes

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -179,8 +179,9 @@ class PageController extends Controller
 
             $this->app = 'blog';
             $this->module = 'blog';
+            $obj->description = 'First Academy is the best coaching center for IELTS, GRE, TOEFL, PTE, OET, SAT,  and other international exams in Hyderabad.'
 
-
+         if(isset($obj->test))
          if($obj->test){
             $try =1;
             $filename = $this->cache_path_test.'test.'.$obj->test.'.json'; 
