d04ef4ee2f19591e7fc57fcebee5f012ad969555
vagrant <vagrant@homestead>
2020-04-07 04:13:15 +0000

    some changes'

--- resources/views/appl/test/attempt/blocks/mcq.blade.php
+++ resources/views/appl/test/attempt/blocks/mcq.blade.php
@@ -18,6 +18,7 @@
       <div class="option">{!! $m->a !!}</div>
   </div>
 </div>
+@if($m->b )
 <div class="row">
     <div class="col-3 col-md-3 col-lg-2">
       <div class="op"><input class
@@ -27,6 +28,9 @@
       <div class="option">{!! $m->b !!}</div>
   </div>
 </div>
+@endif 
+
+@if($m->c )
 <div class="row">
     <div class="col-3 col-md-3 col-lg-2">
       <div class="op"><input class
@@ -36,6 +40,7 @@
       <div class="option">{!! $m->c !!}</div>
   </div>
 </div>
+@endif
 @if($m->d )
 <div class="row">
     <div class="col-3 col-md-3 col-lg-2">
