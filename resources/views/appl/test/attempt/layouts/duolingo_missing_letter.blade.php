2ab66ab8127b05caf8eeec733bd362b68dcdddfb
vagrant <vagrant@homestead>
2020-04-15 16:35:34 +0000

    some changes

--- resources/views/appl/test/attempt/layouts/duolingo_missing_letter.blade.php
+++ resources/views/appl/test/attempt/layouts/duolingo_missing_letter.blade.php
@@ -4,8 +4,8 @@
   @if($f->label ) <div><b>{{$f->label }}</b></div>  @endif 
 
   	
-       @if($f->prefix ) <div style="display:inline-block;margin-top: 5px;float:left">{{$f->prefix }} </div> @endif 
-       @if($f->answer) <div style="display:inline-block; margin-right: 10px;border-radius:5px;margin-left: 5px;margin-top: 5px;float:left" class="{{$open=0}} {{$k=0}} q{{$f->qno}}">
+       @if($f->prefix ) <span>{{$f->prefix }} </span> @endif 
+       @if($f->answer) <span class="{{$open=0}} {{$k=0}} q{{$f->qno}}" style="display: inline-block;margin-left: 5px; margin-right: 5px;">
        @foreach(str_split($f->answer) as $i=>$k)
         @if($k==']' && $open==1)
           <span class="d-none">{{$open=0}}</span> 
@@ -14,8 +14,9 @@
         @elseif($open==0)<input type="text" class="duo duo_{{$i}} input" name="{{$f->qno}}[]" data-id="{{$f->qno}}" value='' maxlength = "1">@elseif($open==1)<input type="text" class="lightb" name="{{$f->qno}}[]" data-id="{{$f->qno}}" value='{{$k}}' disabled><input type="hidden" class="" name="{{$f->qno}}[]" data-id="{{$f->qno}}" value='{{$k}}' >@endif
        @endforeach
        @endif
-     </div>
-       @if($f->suffix )<div style="display:inline-block;margin-top: 5px;float:left">{{$f->suffix }}</div>@endif
+     </span>
+       @if($f->suffix )<div style="display: inline;">
+        <span class="float:left">{{$f->suffix }}</span></div>@endif
     
        
   
