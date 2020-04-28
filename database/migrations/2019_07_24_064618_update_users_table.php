dd0b1377e72f7ea7823089d077ca8365290244e5
vagrant <vagrant@homestead>
2020-04-28 12:55:21 +0000

    some changes

--- database/migrations/2019_07_24_064618_update_users_table.php
+++ database/migrations/2019_07_24_064618_update_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateUsersTable3 extends Migration
+class UpdateUsersTable5 extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2019_09_16_065447_update_users_table-1.php
+++ database/migrations/2019_09_16_065447_update_users_table-1.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateUsersTable1 extends Migration
+class UpdateUsersTable11 extends Migration
 {
     /**
      * Run the migrations.
